9133eeff3425cf56db7cdddc33a6536b6cfb64bc
Mélanie <belard.melanie@gmail.com>
2018-05-15 16:36:03 +0200

    Mise à jour du formulaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 // $("body").click(function(){
-//     alert("ouaaais");
+    // alert("ouaaais");
 // });
+
+if (window.location.hash != '') {
+    $("#register-form "+window.location.hash).show();
+} else {
+    $("#register-form #step-1").show();
+} // À EFFACER
+
+$("#register-form .step-button").click(function(){
+    var currentStep = $(this).data("content");
+    var nextStep    = $(this).attr('href');
+    $("#"+currentStep).hide();
+    $(nextStep).show();
+    console.log(nextStep);
+});
