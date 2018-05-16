b071240236e342c95540f953f6077487526abd4d
Benoit Foucher <benoitfouc@users.noreply.github.com>
2018-05-16 11:41:37 +0200

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,16 +15,3 @@ $("#register-form .step-button").click(function(){
     $(nextStep).show();
     console.log(nextStep);
 });
-
-if (window.location.hash != '') {
-    $("#accueil"+window.location.hash).show();
-} else {
-    $("#accueil #boutonsAccueil").show();
-} // À EFFACER
-
-$("#accueil .step-button").click(function(){
-    var currentStep = $(this).data("content");
-    var nextStep    = $(this).attr('href');
-    $("#"+currentStep).hide();
-    $(nextStep).show();
-});
